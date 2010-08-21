000759fc4da466b9af433c3c0807bd78d6d2e8fb
Yixin Zhu <yixinzhu91@gmail.com>
2010-08-21 02:10:09 -0700

    changed main menu to a stable demo

--- src/com/comic/globals/Globals.java
+++ src/com/comic/globals/Globals.java
@@ -4,39 +4,34 @@ public class Globals {
 	public static String IntroImage="http://samandfuzzy.com/images/vol5_ad.gif";
 	public static String VolZeroImage="http://samandfuzzy.com/imgint/volume0.gif";
 	public static String VolZeroInfo="The first Sam and Fuzzy strips are a little rough " +
-			"around the edges, but are not without their hardcore advocates. This " +
-			"is not the best place for new readers to start, but if you have enjoyed " +
-			"the rest of the archive, dive in here to see the comic's " +
-			"awkward-yet-lovable adolescence.";
+			"around the edges, but are not without their hardcore advocates.";
+	public static String VolZeroName="Volume 0: The Early Years";
 	public static String VolOneImage="http://samandfuzzy.com/imgint/volume1.gif";
 	public static String VolOneInfo="Enter Sam and Fuzzy's surreal world of dating disasters, " +
 			"taxi-driving action superstars, time-travelling bears and possessed" +
 			" refridgerators. Meet many of the series' strangest and most beloved " +
-			"characters in these early strips. This volume is the ideal place to " +
-			"start for any new reader who is ready to tackle the full archive.";
+			"characters in these early strips.";
+	public static String VolOneName="Volume 1: Taxi-cab of Dreams";
 	public static String VolTwoImage="http://samandfuzzy.com/imgint/volume2.gif";
 	public static String VolTwoInfo="After botching a high-profile job and destroying his " +
 			"own taxi-cab, Sam is forced to search for a new career and face some old " +
-			"demons. If you have not read volume one, this brief introduction will tell" +
-			" you everything you need to know about the story so far before you start " +
-			"reading this volume.";
+			"demons.";
+	public static String VolTwoName="Volume 2: Growing Pains";
 	public static String VolThreeImage="http://samandfuzzy.com/imgint/volume3.gif";
 	public static String VolThreeInfo="Working as a bookstore clerk, Sam falls hard for a " +
 			"delivery woman with a terrible secret. Meanwhile, Fuzzy adjusts to his " +
 			"own secret life as a member of the Ninja Mafia crime syndicate's most " +
-			"inept squadran. \"Love and War\" directly continues the story begun in " +
-			"\"Growing Pains\". If you have not read \"Growing Pains\", check out this " +
-			"recap of the story so far before jumping into this volume.";
+			"inept squadran.";
+	public static String VolThreeName="Volume 3: Love and War";
 	public static String VolFourImage="http://samandfuzzy.com/imgint/volume4.gif";
 	public static String VolFourInfo="What happens when the world's most famous hardcore heavy " +
 			"metal vocalist decides he'd rather sing about kittens than corpses? And what " +
 			"does he have to do with our titular duo, anyway? A tale of action, romance, " +
-			"comedy, tragedy, and ninjas. Although \"Noosehead\" is technically a sequel " +
-			"to past stories, it can be enjoyed without any prior knowledge of Sam and " +
-			"Fuzzy and is the perfect jumping-on point for new readers.";
+			"comedy, tragedy, and ninjas.";
+	public static String VolFourName="Volume 4: Noosehead";
 	public static String VolFiveImage="http://samandfuzzy.com/images/vol5.gif";
 	public static String VolFiveInfo="Troubled by ninja mafiosos? Gangster gerbils? Vampire stalkers? " +
 			"Don't worry! Sam and Fuzzy can fix your problem. Now, if only someone could fix " +
-			"theirs... This brand new saga can be enjoyed without even the slightest prior " +
-			"knowledge of Sam and Fuzzy. New readers start here!";
+			"theirs...";
+	public static String VolFiveName="Volume 5: Sam and Fuzzy Fix Your Problem!";
 }
--- src/com/comic/viewer/MainPage.java
+++ src/com/comic/viewer/MainPage.java
@@ -4,32 +4,124 @@ import java.io.InputStream;
 import java.net.URL;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.comic.globals.Globals;
 
 
-public class MainPage extends Activity {
-	private ImageButton intro;
-	private Button volFive;
+public class MainPage extends ListActivity {
+	private String[] volumeNames = {Globals.VolFiveName, 
+			Globals.VolFourName, Globals.VolThreeName, 
+			Globals.VolTwoName, Globals.VolOneName, Globals.VolZeroName};
+	private String[] volumeInfo = {Globals.VolFiveInfo, 
+			Globals.VolFourInfo, Globals.VolThreeInfo, 
+			Globals.VolTwoInfo, Globals.VolOneInfo, Globals.VolZeroInfo};
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		intro = (ImageButton) findViewById(R.id.intro);
-		intro.setImageDrawable(getImage(Globals.IntroImage));
-		volFive = (Button) findViewById(R.id.volfive);
+		setListAdapter(new ListVolumesAdapter(this));
 	}
-	private Drawable getImage(String imageURL){
-		try{
-			InputStream is = (InputStream) new URL(imageURL).getContent();
-			return Drawable.createFromStream(is, "comic image from SMBC");
-		} catch (Exception e){
-			e.printStackTrace();
+	class ListVolumesAdapter extends BaseAdapter implements Filterable {
+		private LayoutInflater mInflater;
+		private Context context;
+
+		public ListVolumesAdapter(Context context) {
+			// Cache the LayoutInflate to avoid asking for a new one each time.
+			mInflater = LayoutInflater.from(context);
+			this.context = context;
 		}
-		return null;
+
+		/**
+		 * Make a view to hold each row.
+		 * 
+		 * @see android.widget.ListAdapter#getView(int, android.view.View,
+		 *      android.view.ViewGroup)
+		 */
+		public View getView(final int position, View convertView,
+				ViewGroup parent) {
+			// A ViewHolder keeps references to children views to avoid
+			// unneccessary calls to findViewById() on each row.
+			ViewHolder holder;
+			// When convertView is not null, we can reuse it directly, there is
+			// no need to reinflate it. We only inflate a new View when the
+			// convertView
+			// supplied by ListView is null.
+			if (convertView == null) {
+				convertView = mInflater.inflate(R.layout.main_context,
+						null);
+				// Creates a ViewHolder and store references to the two children
+				// views we want to bind data to.
+				holder = new ViewHolder();
+				holder.volumeName = (TextView) convertView
+						.findViewById(R.id.volumeName);
+				holder.volumeDescription = (TextView) convertView
+						.findViewById(R.id.volumeDiscription);
+
+				convertView.setOnClickListener(new OnClickListener() {
+					private int pos = position;
+
+					public void onClick(View v) {
+						// Toast.makeText(context, "Click-" +
+						// String.valueOf(pos), Toast.LENGTH_SHORT).show();
+						//getSurveysFromPyramid(merchlist.get(pos));
+						
+						//TODO: display proper image
+					}
+				});
+				convertView.setTag(holder);
+			} else {
+				// Get the ViewHolder back to get fast access to the TextView
+				// and the ImageView.
+				holder = (ViewHolder) convertView.getTag();
+			}
+			holder.volumeName.setText(volumeNames[position]);
+			holder.volumeDescription
+					.setText(volumeInfo[position]);
+			return convertView;
+		}
+
+		class ViewHolder {
+			TextView volumeName;
+			TextView volumeDescription;
+
+		}
+
+		public Filter getFilter() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return volumeNames.length;
+		}
+
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return volumeNames[position];
+		}
+
 	}
 }
