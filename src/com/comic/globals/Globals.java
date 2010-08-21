0c94bdca03436381ac0ec2199bc1bcb64f103d9b
Yixin Zhu <yixinzhu91@gmail.com>
2010-08-21 01:08:22 -0700

    first test design of main page

--- src/com/comic/globals/Globals.java
+++ src/com/comic/globals/Globals.java
@@ -1,26 +1,41 @@
 package com.comic.globals;
 
 public class Globals {
-	public String IntroLinkToRecentVol="http://samandfuzzy.com/images/vol5_ad.gif";
-	public String VolZeroImage="http://samandfuzzy.com/imgint/volume0.gif";
-	public String VolOneImage="http://samandfuzzy.com/imgint/volume1.gif";
-	public String VolTwoImage="http://samandfuzzy.com/imgint/volume2.gif";
-	public String VolThreeImage="http://samandfuzzy.com/imgint/volume3.gif";
-	public String VolThreeInfo="Working as a bookstore clerk, Sam falls hard for a " +
+	public static String IntroImage="http://samandfuzzy.com/images/vol5_ad.gif";
+	public static String VolZeroImage="http://samandfuzzy.com/imgint/volume0.gif";
+	public static String VolZeroInfo="The first Sam and Fuzzy strips are a little rough " +
+			"around the edges, but are not without their hardcore advocates. This " +
+			"is not the best place for new readers to start, but if you have enjoyed " +
+			"the rest of the archive, dive in here to see the comic's " +
+			"awkward-yet-lovable adolescence.";
+	public static String VolOneImage="http://samandfuzzy.com/imgint/volume1.gif";
+	public static String VolOneInfo="Enter Sam and Fuzzy's surreal world of dating disasters, " +
+			"taxi-driving action superstars, time-travelling bears and possessed" +
+			" refridgerators. Meet many of the series' strangest and most beloved " +
+			"characters in these early strips. This volume is the ideal place to " +
+			"start for any new reader who is ready to tackle the full archive.";
+	public static String VolTwoImage="http://samandfuzzy.com/imgint/volume2.gif";
+	public static String VolTwoInfo="After botching a high-profile job and destroying his " +
+			"own taxi-cab, Sam is forced to search for a new career and face some old " +
+			"demons. If you have not read volume one, this brief introduction will tell" +
+			" you everything you need to know about the story so far before you start " +
+			"reading this volume.";
+	public static String VolThreeImage="http://samandfuzzy.com/imgint/volume3.gif";
+	public static String VolThreeInfo="Working as a bookstore clerk, Sam falls hard for a " +
 			"delivery woman with a terrible secret. Meanwhile, Fuzzy adjusts to his " +
 			"own secret life as a member of the Ninja Mafia crime syndicate's most " +
 			"inept squadran. \"Love and War\" directly continues the story begun in " +
 			"\"Growing Pains\". If you have not read \"Growing Pains\", check out this " +
 			"recap of the story so far before jumping into this volume.";
-	public String VolFourImage="http://samandfuzzy.com/imgint/volume4.gif";
-	public String VolFourInfo="What happens when the world's most famous hardcore heavy " +
+	public static String VolFourImage="http://samandfuzzy.com/imgint/volume4.gif";
+	public static String VolFourInfo="What happens when the world's most famous hardcore heavy " +
 			"metal vocalist decides he'd rather sing about kittens than corpses? And what " +
 			"does he have to do with our titular duo, anyway? A tale of action, romance, " +
 			"comedy, tragedy, and ninjas. Although \"Noosehead\" is technically a sequel " +
 			"to past stories, it can be enjoyed without any prior knowledge of Sam and " +
 			"Fuzzy and is the perfect jumping-on point for new readers.";
-	public String VolFiveImage="http://samandfuzzy.com/images/vol5.gif";
-	public String VolFiveInfo="Troubled by ninja mafiosos? Gangster gerbils? Vampire stalkers? " +
+	public static String VolFiveImage="http://samandfuzzy.com/images/vol5.gif";
+	public static String VolFiveInfo="Troubled by ninja mafiosos? Gangster gerbils? Vampire stalkers? " +
 			"Don't worry! Sam and Fuzzy can fix your problem. Now, if only someone could fix " +
 			"theirs... This brand new saga can be enjoyed without even the slightest prior " +
 			"knowledge of Sam and Fuzzy. New readers start here!";
